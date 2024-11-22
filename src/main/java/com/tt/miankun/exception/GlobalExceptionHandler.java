5a1a2ab2122934a1801ee5fbb5d1992f96ee2900
tt <songtaoxie@hotmail.com>
2024-11-22 20:38:39 +0800

    feat: 项目模板初始化

---
+++ src/main/java/com/tt/miankun/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.tt.miankun.exception;
+
+import com.tt.miankun.common.BaseResponse;
+import com.tt.miankun.common.ErrorCode;
+import com.tt.miankun.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ src/main/java/com/tt/miankun/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.tt.miankun.mapper;
+
+import com.tt.miankun.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
